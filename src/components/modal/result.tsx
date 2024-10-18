36db11081e2e986e88106413180c8ef06fe7f72a
HappyRio <happyrio1013@gmail.com>
2024-10-18 16:25:32 +0900

    fix alert style

--- src/components/modal/result.tsx
+++ src/components/modal/result.tsx
@@ -2,7 +2,7 @@
 
 const OperationResult = ({text}:{ text:string }) => {
     return(
-        <div className="fixed top-1/3 left-1/2 -translate-x-1/2 px-12 py-2 rounded-xl bg-black bg-opacity-30 text-base text-white">{text}</div>
+        <div className="fixed top-1/3 left-1/2 -translate-x-1/2 px-12 py-2 rounded-xl bg-black bg-opacity-50 text-base text-white text-nowrap">{text}</div>
     )
 }
 
